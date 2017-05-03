ef5b4504f35737f9faaa16286e1500906140252e
DrewWarrenTIY <drewwarrentiy@gmail.com>
2017-05-03 10:38:09 -0600

    Fun stuff

--- scripts.js
+++ scripts.js
@@ -4,35 +4,35 @@ $('body').on('click', '.js-slide-up', function () {
 })
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 2000) {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 2500) {
     $('.js-slider-one').removeClass('slide-up');
     $('.js-slider-one').addClass('no-slide');
   }
 });
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 1650) {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 2150) {
     $('.js-slider-two').removeClass('slide-up');
     $('.js-slider-two').addClass('no-slide');
   }
 });
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 1300) {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 1800) {
     $('.js-slider-three').removeClass('slide-up');
     $('.js-slider-three').addClass('no-slide');
   }
 });
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 950) {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 1450) {
     $('.js-slider-four').removeClass('slide-up');
     $('.js-slider-four').addClass('no-slide');
   }
 });
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 600) {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 1100) {
     $('.js-slider-five').removeClass('slide-up');
     $('.js-slider-five').addClass('no-slide');
   }
